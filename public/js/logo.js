90ec691226aadcb53e57e9362f255fee0fcc0149
hilaryohilary <70475850+hilaryohilary@users.noreply.github.com>
2021-10-16 11:47:40 +0100

    made major changes to the layouts

---
+++ public/js/logo.js
@@ -0,0 +1,9 @@
+
+const logoAnimation = document.querySelector('#logo-animation');
+const app = document.querySelector('app');
+
+setTimeout(() => {
+logoAnimation.style.opacity  = 0;
+logoAnimation.style.display = 'none';
+app.style.display = 'block';
+}, 3000);
